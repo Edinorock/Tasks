74a1930af0ac040436c22ee61109f7d5451e0699
Edinorock <111507622+Edinorock@users.noreply.github.com>
2023-03-01 16:11:24 +0300

    Commit

---
+++ src/User.java
@@ -0,0 +1,32 @@
+public class User {
+    private final String name;
+    private final int age;
+    private final String phoneNumber;
+
+    public User(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
